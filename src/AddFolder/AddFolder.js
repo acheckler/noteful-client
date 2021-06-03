b384d303cc46488c956bcc92ed475564e2d7754a
acheckler <allisonheckler@gmail.com>
2021-06-03 17:25:33 -0500

    fixed addFolder routing

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -1,26 +1,47 @@
 import React, { Component } from "react";
-import ApiContext from '../ApiContext'
-import PropTypes from 'prop-types'
-
-
+import ApiContext from "../ApiContext";
+import PropTypes from "prop-types";
+import config from "../config";
 
 class AddFolder extends Component {
-    static contextType = ApiContext
-    static propTypes = {
-      history: PropTypes.object
-    }
-    handleSubmit(event) {
+  static contextType = ApiContext;
+  static propTypes = {
+    history: PropTypes.object,
+  };
+
+  handleSubmit(event) {
     event.preventDefault();
-    const folderName = event.target.name.value
-    this.context.addFolder(folderName)
-    this.props.history.push(`/`)
-    // process form values here
+    const folderName = event.target.name.value;
+    this.handleAddFolder(folderName);
   }
 
+  handleAddFolder = (folderName) => {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ folder_name: folderName }),
+    };
+
+    fetch(`${config.API_ENDPOINT}/folders/`, options)
+      .then((folderRes) => {
+        if (!folderRes.ok) {
+          throw new Error("error");
+        }
+
+        return folderRes.json();
+      })
+      .then((folderRes) => {
+        const folderId = folderRes.id;
+        this.props.history.push(`/folder/${folderId}`);
+      })
+      .catch((error) => console.log(error));
+  };
 
   render() {
     return (
-      <form className="AddFolder" onSubmit={e => this.handleSubmit(e)}>
+      <form className="AddFolder" onSubmit={(e) => this.handleSubmit(e)}>
         <h2>Create A Folder</h2>
         <div className="folderForm">
           <label htmlFor="name">Name:</label>
@@ -33,7 +54,7 @@ class AddFolder extends Component {
           />
         </div>
         <div className="create-folder-button">
-            <button type="submit" className="registration__button">
+          <button type="submit" className="registration__button">
             Save
           </button>
         </div>
@@ -41,4 +62,4 @@ class AddFolder extends Component {
     );
   }
 }
- export default AddFolder
+export default AddFolder;
--- src/config.js
+++ src/config.js
@@ -1,5 +1,3 @@
 export default {
     API_ENDPOINT: 'https://mysterious-tundra-86504.herokuapp.com/api'
   }
-
-  //noteful-json-server  
